44bb4cab43cfd8a49e11e3af873110ebc0734c8f
ReedStuhl <reedstuhlreyer@gmail.com>
2023-05-25 22:09:55 -0400

    lost a lot of code picking up the pieces now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from
 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import MediaCard from './components/Card.js'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' exact />
       </Routes>
       </Router>
+      <MediaCard/>
     </>
   );
 }
