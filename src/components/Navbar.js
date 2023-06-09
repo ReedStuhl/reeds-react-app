c0150adf2902ea84507e94a9b2b5927e810ff68b
ReedStuhl <reedstuhlreyer@gmail.com>
2023-06-08 23:19:18 -0400

    fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,6 @@ function Navbar() {
     const handleClick = ()=> setClick(!click)
     const closeMobileMenu = () => setClick(false)
 
-    const showButton = () => {
-        if(window.innerWidth <= 960){
-            setButton(false)
-        } else {
-            setButton(true)
-        }
-    }
-
     window.addEventListener('resize', showButton)
 
   return (
