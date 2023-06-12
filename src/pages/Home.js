46220ee0d7d2e9a0800c6eb13df0e845d15947cb
ReedStuhl <reedstuhlreyer@gmail.com>
2023-06-12 15:34:15 -0400

    new Yellow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Home.css';
 import headShot from '../images/plsWork.png';
-import yellowstone from '../images/Yellowstone.jpeg';
 
 function Home() {
     return (
