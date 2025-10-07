92799529486c2608d21a71aeeeda697a5afc2311
Reed Stuhlreyer <reedstuhlreyer@Mac.hsd1.mi.comcast.net>
2025-10-07 15:46:57 -0400

    footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
                 <div>
                     <a className="socials" href='https://www.linkedin.com/in/reed-stuhlreyer/' target="_blank" rel="noreferrer"> <i className='fa-brands fa-linkedin fa-xl' ></i></a> 
                     <a className="socials" href='https://github.com/ReedStuhl' target="_blank" rel="noreferrer" color="white"><i className='fa-brands fa-github fa-xl'/></a>
-                    <a href="mailto:reedstuhlreyer@gmail.com">reedstuhlreyer@gmail.com</a>
+                    <a className="gmail" href="mailto:reedstuhlreyer@gmail.com">reedstuhlreyer@gmail.com</a>
                 </div>
             </div>
         </div>
